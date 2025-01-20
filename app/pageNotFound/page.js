67c567cdc543429beed12f83d82368d95d5d8ee9
Franco <francograndedela@gmail.com>
2025-01-19 21:55:54 -0300

    boton volver en 404

--- app/pageNotFound/page.js
+++ app/pageNotFound/page.js
@@ -14,7 +14,7 @@ export default function ErrorPage() {
         <Navbar />
 
         <img src="elmoOnFire.jpg" alt="404" className='w-full h-full object-cover'/>
-        <button className='absolute top-48  left-1/2 transform -translate-x-1/2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-full text-5xl' >Volver</button>
+        <button className='absolute top-48  left-1/2 transform -translate-x-1/2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-full text-5xl' onClick={() => window.history.back()} >Volver</button>
         <Footer />
         </>
     )

91b6960c022b566099ab44989c92ab8ad5882282
Franco <francograndedela@gmail.com>
2025-01-19 21:54:47 -0300

    agregando a ElmoError404

--- app/pageNotFound/page.js
+++ app/pageNotFound/page.js
@@ -13,8 +13,8 @@ export default function ErrorPage() {
         <>
         <Navbar />
 
-        <h1>404</h1>
-
+        <img src="elmoOnFire.jpg" alt="404" className='w-full h-full object-cover'/>
+        <button className='absolute top-48  left-1/2 transform -translate-x-1/2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-full text-5xl' >Volver</button>
         <Footer />
         </>
     )

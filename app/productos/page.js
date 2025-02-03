03255f195763a9baea299421f7aece06ded9f2e9
Franco <francograndedela@gmail.com>
2025-02-03 00:31:03 -0300

    mas de lo mismo

--- app/productos/page.js
+++ app/productos/page.js
@@ -83,21 +83,23 @@ return (
             </button>
 
         </div>
-            <div className="flex justify-center pt-10 items-start">
-                <div className="md:grid-flow-col	 lg: bg-white flex flex-wrap items-center justify-between space-x-10 p-10 m-10 w-96 m-4">
+
+        {/* lista de categorias en en barra lateral izquieda */}
+            <div className="flex justify-center pt-10 items-start py-8">
+                <div className="grid grid-cols-2 gap-4 bg-white p-6 w-80 rounded-lg shadow-lg">
                     <button onClick={() => handleCategoryClick(null)} className="hover:bg-blue-500 bg-gray-800 hover:bg-blue-500 py-2 px-4 rounded-lg font-normal hover:font-bold text-2xl hover:font-semibold"><i className="fab fa-facebook"></i>Todos</button>
                     <button onClick={() => handleCategoryClick('samsung')} className=" hover:bg-blue-500 bg-gray-800 hover:bg-blue-500 py-2 px-4 rounded-lg font-normal hover:font-bold text-2xl hover:font-semibold"><i className="fab fa-facebook"></i>Samsung</button>
-                    <button onClick={() => handleCategoryClick('motorola')} className="m-2 hover:bg-blue-500 bg-gray-800 hover:bg-blue-500 py-2 px-4 rounded-lg font-normal hover:font-bold text-2xl hover:font-semibold"><i className="fab fa-instagram"></i>Motorola</button>
+                    <button onClick={() => handleCategoryClick('motorola')} className=" hover:bg-blue-500 bg-gray-800 hover:bg-blue-500 py-2 px-4 rounded-lg font-normal hover:font-bold text-2xl hover:font-semibold"><i className="fab fa-instagram"></i>Motorola</button>
                     <button onClick={() => handleCategoryClick('zte')} className="hover:bg-blue-500 bg-gray-800 hover:bg-blue-500 py-2 px-4 rounded-lg font-normal hover:font-bold text-2xl hover:font-semibold"><i className="fab fa-github"></i>ZTE</button>
                     <button onClick={() =>handleCategoryClick('xiaomi')} className="hover:bg-blue-500 bg-gray-800 hover:bg-blue-500 py-2 px-4 rounded-lg font-normal hover:font-bold text-2xl hover:font-semibold"><i className="fab fa-github"></i>Xiaomi</button>
                     <button onClick={() =>handleCategoryClick('tcl')} className=" hover:bg-blue-500 bg-gray-800 hover:bg-blue-500 py-2 px-4 rounded-lg font-normal hover:font-bold text-2xl hover:font-semibold "><i className="fab fa-youtube"></i>TCL</button>
                 </div>
 
 
-
+        {/* lista de productos mapeando base de datos */}
                 <div className="md:grid-flow-col	 lg:flex items-center justify-center space-x-10">
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4">
                 {
                     filteredProducts.map((product) => (
                             <div key={product.slug} className="bg-white border border-gray-300 rounded-lg shadow-lg p-4 w-80">
